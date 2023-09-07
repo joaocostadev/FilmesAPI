a6c7bb72779a5a6b28a1b461deab6cdfd18e571f
João Paulo <54214033+joaocostadev@users.noreply.github.com>
2023-09-07 14:04:13 -0300

    Salvando filmes no banco mysql

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -1,4 +1,5 @@
-﻿using FilmesApi.Models;
+﻿using FilmesApi.Data;
+using FilmesApi.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FilmesApi.Controllers
@@ -8,27 +9,32 @@ namespace FilmesApi.Controllers
     public class FilmeController : ControllerBase
     {
 
-        private static List<Filme> filmes = new List<Filme>();
-        private static int id = 0;
+        private FilmeContext _context;
+
+        public FilmeController(FilmeContext context)
+        {
+            _context = context;
+        }
 
         [HttpPost]
         public IActionResult AdicionaFilme([FromBody] Filme filme)
         {
-            filme.Id = id++;
-            filmes.Add(filme);
+
+            _context.Filmes.Add(filme);
+            _context.SaveChanges();
             return CreatedAtAction(nameof(RecuperaFilmePorId), new { id = filme.Id }, filme);
 
         }
         [HttpGet]
         public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50)
         {
-            return filmes.Skip(skip).Take(take);
+            return _context.Filmes.Skip(skip).Take(take);
         }
 
         [HttpGet("{id}")]
         public IActionResult RecuperaFilmePorId(int id) 
         {
-            var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+            var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
             if (filme == null) return NotFound();
             return Ok(filme);
         }
