ccc08e0bdf9fc522b9652aa8893b94f664c6073a
João Paulo <54214033+joaocostadev@users.noreply.github.com>
2023-09-06 21:28:42 -0300

    Buscar Filme por ID

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -9,10 +9,12 @@ namespace FilmesApi.Controllers
     {
 
         private static List<Filme> filmes = new List<Filme>();
+        private static int id = 0;
 
         [HttpPost]
         public void AdicionaFilme([FromBody] Filme filme)
         {
+            filme.Id = id++;
             filmes.Add(filme);
             Console.WriteLine(filme.Titulo);
             Console.WriteLine(filme.Duracao);
@@ -21,6 +23,12 @@ namespace FilmesApi.Controllers
         public IEnumerable<Filme> RecuperaFilmes()
         {
             return filmes;
+        }
+
+        [HttpGet("{id}")]
+        public Filme? RecuperaFilmePorId(int id) 
+        {
+            return filmes.FirstOrDefault(filme => filme.Id == id);
         }
     }
 }
